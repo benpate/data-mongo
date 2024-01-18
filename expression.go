3a7fb8cc013506184fe70344408d1f0cb989c55a
Ben Pate <ben@pate.org>
2024-01-17 18:28:59 -0700

    Use Case-Insensitive comparisons for CONTAINS, BEGINS, ENDS

--- expression.go
+++ expression.go
@@ -3,6 +3,7 @@ package mongodb
 import (
 	"github.com/benpate/exp"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // ExpressionToBSON converts a data.Expression value into pure bson.
@@ -13,7 +14,7 @@ func ExpressionToBSON(criteria exp.Expression) bson.M {
 	case exp.Predicate:
 
 		result := bson.M{}
-		result[c.Field] = bson.M{operatorBSON(c.Operator): c.Value}
+		result[c.Field] = operatorBSON(c.Operator, c.Value)
 		return result
 
 	case exp.AndExpression:
@@ -49,26 +50,51 @@ func ExpressionToBSON(criteria exp.Expression) bson.M {
 }
 
 // operatorBSON converts a standard data.Operator into the operators used by mongodb
-func operatorBSON(operator string) string {
+func operatorBSON(operator string, value any) bson.M {
 
 	switch operator {
 	case exp.OperatorEqual:
-		return "$eq"
+		return bson.M{"$eq": value}
+
 	case exp.OperatorNotEqual:
-		return "$ne"
+		return bson.M{"$ne": value}
+
 	case exp.OperatorLessThan:
-		return "$lt"
+		return bson.M{"$lt": value}
+
 	case exp.OperatorLessOrEqual:
-		return "$lte"
+		return bson.M{"$lte": value}
+
 	case exp.OperatorGreaterOrEqual:
-		return "$gte"
+		return bson.M{"$gte": value}
+
 	case exp.OperatorGreaterThan:
-		return "$gt"
+		return bson.M{"$gt": value}
+
 	case exp.OperatorIn:
-		return "$in"
+		return bson.M{"$in": value}
+
 	case exp.OperatorNotIn:
-		return "$nin"
+		return bson.M{"$nin": value}
+
+	case exp.OperatorBeginsWith:
+		if valueString, ok := value.(string); ok {
+			return bson.M{"$regex": primitive.Regex{Pattern: "^" + valueString, Options: "i"}}
+		}
+
+	case exp.OperatorContains:
+		if valueString, ok := value.(string); ok {
+			return bson.M{"$regex": primitive.Regex{Pattern: valueString, Options: "i"}}
+		}
+
+	case exp.OperatorEndsWith:
+		if valueString, ok := value.(string); ok {
+			return bson.M{"$regex": primitive.Regex{Pattern: valueString + "$", Options: "i"}}
+		}
+
 	default:
-		return "$eq"
+		return bson.M{"$eq": value}
 	}
+
+	return bson.M{}
 }
