1bac02ff834e1c3aee9e092023605b5cc09cc5a4
Ben Pate <ben@pate.org>
2026-06-27 16:11:17 -0600

    Remove dead code

--- geo.go
+++
@@ -1,9 +0,0 @@
-package mongodb
-
-// GeoJSONer interface wraps the GeoJSON method, which generates
-// a GeoJSON object for a given dataset
-type GeoJSONer interface {
-
-	// GeoJSON returns a GeoJSON representation of a geometric shape.
-	GeoJSON() map[string]any
-}
